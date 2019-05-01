3589465374c380effd0ae7ddb163816e94a0f2b6
Saha <windcrack@mail.ru>
2019-05-01 12:24:54 +0300

    add new param scroll menu

--- src/js/module/menuSlider.js
+++ src/js/module/menuSlider.js
@@ -1,58 +1,45 @@
-
-export default function menuSlider (){
-    // Это старые настройки меню.
-    $('.text-slider').slick({
-        arrows: false,
-        variableWidth: true,
-        dots: false,
-        infinite: false,
-        centerPadding: '0px',
-        responsive:[
-            {
-                breakpoint: 600,
-                settings:{
-                    adaptiveHeight: true,
-                    arrows: false,
-                    variableWidth: true,
-                    dots: false,
-                    initialSlide: 0,
-                    slidesToShow: 3,
-                    slideToScroll: 1,
-                    centerPadding: '0px',
-                }
-            }
-        ]
-      });
+export default function menuSlider() {
     // -----------------------------------------
     // Это новое боковое меню.
     const menuLeft = document.querySelector('.leftMenuBlock'),
-          inMenu = document.querySelector('.leftMenuMain'),
-          btnOpen = document.querySelector('.btn-side-open'),
-          headerBlock = document.querySelector('.menuHeader'),
-          siteBlock = document.querySelectorAll('.menu-section'),
-          btnClose = document.querySelector('.btn-side-close'),
-          btnCloseMob = document.querySelector('.btn-side-closeMob'),
-          btnCloseMobEl = document.querySelector('.secretBlock .btn-side-closeMob i');
-    let width = innerWidth
+        inMenu = document.querySelector('.leftMenuMain'),
+        btnOpen = document.querySelector('.btn-side-open'),
+        headerBlock = document.querySelector('.menuHeader'),
+        siteBlock = document.querySelectorAll('.menu-section'),
+        btnClose = document.querySelector('.btn-side-close'),
+        btnCloseMob = document.querySelector('.btn-side-closeMob'),
+        btnCloseMobEl = document.querySelector('.secretBlock .btn-side-closeMob i');
+    let width = innerWidth;
     // Обект появелниея блоков по очеродности.
     const showTime = {
-        menu: function(){
-            setTimeout(() =>{ inMenu.style.display = 'flex' }, 100)
+        menu: function () {
+            setTimeout(() => {
+                inMenu.style.display = 'flex'
+            }, 100)
         },
-        headerBlock: function(){
-            setTimeout(() =>{ headerBlock.style.display = 'flex' }, 800)
+        headerBlock: function () {
+            setTimeout(() => {
+                headerBlock.style.display = 'flex'
+            }, 800)
         },
-        siteBlock: function(){
-            setTimeout(() =>{ siteBlock.forEach(item =>{ item.style.display = 'flex' }) }, 1200)
+        siteBlock: function () {
+            setTimeout(() => {
+                siteBlock.forEach(item => {
+                    item.style.display = 'flex'
+                })
+            }, 1200)
         },
-        hiddeAll: function (){
+        hiddeAll: function () {
             inMenu.style.display = 'none'
             headerBlock.style.display = 'none'
-            siteBlock.forEach(item =>{ item.style.display = 'none' })
+            siteBlock.forEach(item => {
+                item.style.display = 'none'
+                
+            })
         }
     }
     // фунция зарытия открытия меню и всех его частей
-    function showLeftSideMenu (open, close, closeMob, closeMobE, menu){
+    function showLeftSideMenu(open, close, closeMob, closeMobE, menu) {
         open.addEventListener('click', () => {
             menu.style.display = 'block'
             close.style.display = 'block'
@@ -62,15 +49,17 @@ export default function menuSlider (){
             showTime.headerBlock()
             showTime.menu()
         })
-        close.addEventListener('click', () =>{
+
+        close.addEventListener('click', () => {
             open.style.display = 'block'
             menu.style.display = 'none'
             close.style.display = 'none'
             closeMob.style.display = 'none'
             showTime.hiddeAll()
         })
-        if(width <= 600){
-            closeMobE.addEventListener('click', () =>{
+
+        if (width <= 600) {
+            closeMobE.addEventListener('click', () => {
                 open.style.display = 'block'
                 menu.style.display = 'none'
                 close.style.display = 'none'
